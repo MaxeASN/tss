30410754b76fef2ecd3c7dc5dd60a2a18f9538a1
teslapatrick <teslapatrick@gmail.com>
2023-10-18 17:31:28 +0800

    add: worker pool

---
+++ worker/config.go
@@ -0,0 +1,20 @@
+package worker
+
+import (
+	flag "github.com/spf13/pflag"
+)
+
+const DefaultWorkerQueueSize = 10
+const DefaultWorkerTimeout = "10s"
+
+type WorkerConfig struct {
+	WorkerLimit int `koanf:"limit"`
+}
+
+var DefaultWorkerConfig = WorkerConfig{
+	WorkerLimit: DefaultWorkerQueueSize,
+}
+
+func WorkerConfigAddOptions(prefix string, f *flag.FlagSet) {
+	f.Int(prefix+".limit", DefaultWorkerConfig.WorkerLimit, "Number of workers")
+}
